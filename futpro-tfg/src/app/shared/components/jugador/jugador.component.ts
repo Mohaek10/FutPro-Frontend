32f5fefb73e8b4f7c0d839f3aa96a27749a7a891
Mohaek10 <elkasmimoha@gmail.com>
2024-06-04 13:01:40 +0200

    Add components and jugador, equipo models

---
+++ futpro-tfg/src/app/shared/components/jugador/jugador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador',
+  standalone: true,
+  imports: [],
+  templateUrl: './jugador.component.html',
+  styleUrl: './jugador.component.css'
+})
+export class JugadorComponent {
+
+}
