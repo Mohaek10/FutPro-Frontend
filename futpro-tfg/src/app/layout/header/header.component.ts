ad1c523ea1d8fd7f156ace848c45dd0e513848ec
Mohaek10 <elkasmimoha@gmail.com>
2024-06-04 23:19:50 +0200

    Add header

--- futpro-tfg/src/app/layout/header/header.component.ts
+++ futpro-tfg/src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
