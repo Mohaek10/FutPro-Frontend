01028781f36965fcda440ddb3ee4845e1314cbaa
Mohaek10 <elkasmimoha@gmail.com>
2024-06-06 13:24:55 +0200

    Ajustes de jeader

--- futpro-tfg/src/app/app.config.ts
+++ futpro-tfg/src/app/app.config.ts
@@ -7,5 +7,5 @@ import {provideHttpClient} from "@angular/common/http";
 import {provideClientHydration} from "@angular/platform-browser";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideClientHydration()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
 };
