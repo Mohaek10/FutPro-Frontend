2ea4c2550b3a07b921282d1639e5f5ae06d3951a
Mohaek10 <elkasmimoha@gmail.com>
2024-06-07 23:02:38 +0200

    Servicio de jugadores

--- futpro-tfg/src/app/core/services/jugadores.service.ts
+++ futpro-tfg/src/app/core/services/jugadores.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Jugador} from "../../shared/models/jugador.models";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JugadoresService {
+  private apiUrl = 'http://localhost:8000/api/jugadores/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getJugadores(search?: string, filters?: any): Observable<{ count: number, results: Jugador[] }> {
+    let params = new HttpParams();
+    if (search) {
+      params = params.set('search', search);
+    }
+    if (filters) {
+      Object.keys(filters).forEach(key => {
+        params = params.set(key, filters[key]);
+      });
+    }
+    return this.http.get<{ count: number, results: Jugador[] }>(this.apiUrl, {params});
+  }
+
+  getAllJugadores(): Observable<Jugador[]> {
+    return this.http.get<Jugador[]>(this.apiUrl);
+
+  }
+
+  getJugador(id: number): Observable<Jugador> {
+    return this.http.get<Jugador>(`${this.apiUrl}${id}/`);
+  }
 }
