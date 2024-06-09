5c694c509cfe68ee0eecde5bde2e2947ecb8cf42
Mohaek10 <elkasmimoha@gmail.com>
2024-06-09 20:41:22 +0200

    Adicion de interceptor para errores de servidor

--- futpro-tfg/src/app/app.config.ts
+++ futpro-tfg/src/app/app.config.ts
@@ -5,7 +5,8 @@ import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./core/interceptors/auth.interceptor";
+import {errorInterceptor} from "./core/interceptors/error.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))]
 };
