1737b31f227267db5537a83be6317c76a9954c48
Mohaek10 <elkasmimoha@gmail.com>
2024-06-09 22:42:55 +0200

    Quitamos interceptor malo

--- futpro-tfg/src/app/app.config.ts
+++ futpro-tfg/src/app/app.config.ts
@@ -5,8 +5,7 @@ import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./core/interceptors/auth.interceptor";
-import {errorInterceptor} from "./core/interceptors/error.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor]))]
 };
