c728404b78910c7ec3ab0ffd55e7232786a5ae61
Mohaek10 <elkasmimoha@gmail.com>
2024-06-10 11:03:38 +0200

    Mercado sis list

--- futpro-tfg/src/app/app.routes.ts
+++ futpro-tfg/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {RegisterComponent} from "./features/auth/register/register.component";
 import {JugadorListComponent} from "./features/jugadores/jugador-list/jugador-list.component";
 import {authGuard} from "./core/guards/auth.guard";
 import {JugadorDetailComponent} from "./features/jugadores/jugador-detail/jugador-detail.component";
+import {MercadoSisComponent} from "./features/mercado/mercado-sis/mercado-sis.component";
 
 export const routes: Routes = [
   {path: '', component: DashboardComponent},
@@ -18,4 +19,5 @@ export const routes: Routes = [
   {path: 'admin/users', component: ManageUsersComponent, canActivate: [authGuard, adminGuard]},
   {path: 'jugadores', component: JugadorListComponent},
   {path: 'jugador/:id', component: JugadorDetailComponent},
+  {path: 'mercado-sis', component: MercadoSisComponent},
 ];
--- futpro-tfg/src/app/features/mercado/mercado-sis/mercado-sis.component.ts
+++ futpro-tfg/src/app/features/mercado/mercado-sis/mercado-sis.component.ts
@@ -1,14 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {Jugador} from "../../../shared/models/jugador.models";
-import {FormControl, FormGroup} from "@angular/forms";
-import {JugadoresService} from "../../../core/services/jugadores.service";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MercadoService} from "../../../core/services/mercado.service";
-import {PageEvent} from "@angular/material/paginator";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {JugadorComponent} from "../../../shared/components/jugador/jugador.component";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-mercado-sis',
   standalone: true,
-  imports: [],
+  imports: [
+    JugadorComponent,
+    MatButtonToggle,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatPaginator,
+    MatSelect,
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './mercado-sis.component.html',
   styleUrl: './mercado-sis.component.css'
 })
@@ -32,6 +50,17 @@ export class MercadoSisComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getMercadoSisList();
+
+    this.searchControl.valueChanges.subscribe(value => {
+      this.pageIndex = 0;
+      this.getMercadoSisList(value ?? undefined, this.filterForm.value);
+    });
+
+    this.filterForm.valueChanges.subscribe(value => {
+      this.pageIndex = 0;
+      this.getMercadoSisList(this.searchControl.value ?? undefined, value);
+    });
 
   }
 
