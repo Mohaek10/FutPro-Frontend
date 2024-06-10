7c0c9ba93adc1b1412922fafb4c97bc24a02f997
Mohaek10 <elkasmimoha@gmail.com>
2024-06-10 22:02:42 +0200

    Mercado User primera version

--- futpro-tfg/src/app/core/services/mercado.service.ts
+++ futpro-tfg/src/app/core/services/mercado.service.ts
@@ -40,6 +40,34 @@ export class MercadoService {
     }>(endPoints, {params});
   }
 
+  getMercadoUsuario(search?: string, filters?: any, page: number = 1, pageSize: number = 10): Observable<{
+    count: number,
+    next: string | null,
+    previous: string | null,
+    results: any[]
+  }> {
+    let params = new HttpParams();
+    if (search) {
+      params = params.set('search', search);
+    }
+    if (filters) {
+      Object.keys(filters).forEach(key => {
+        if (filters[key]) {
+          params = params.set(key, filters[key]);
+        }
+      });
+    }
+    params = params.set('limit', pageSize.toString());
+    params = params.set('offset', ((page - 1) * pageSize).toString());
+    const endPoints = this.apiUrl.concat('mercado-usuarios/')
+    return this.http.get<{
+      count: number,
+      next: string | null,
+      previous: string | null,
+      results: any[]
+    }>(endPoints, {params});
+  }
+
   comprarJugadorSistema(jugador_id: number, cantidad: number): Observable<any> {
     const endPoints = this.apiUrl.concat('comprar-sistema/');
     return this.http.post(endPoints, {jugador_id, cantidad});
--- futpro-tfg/src/app/features/mercado/mercado-use/mercado-use.component.ts
+++ futpro-tfg/src/app/features/mercado/mercado-use/mercado-use.component.ts
@@ -1,12 +1,90 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Jugador} from "../../../shared/models/jugador.models";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MercadoService} from "../../../core/services/mercado.service";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {JugadorComponent} from "../../../shared/components/jugador/jugador.component";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+import {FormatoNumeroPipe} from "../../../shared/pipes/formato-numero.pipe";
 
 @Component({
   selector: 'app-mercado-use',
   standalone: true,
-  imports: [],
+  imports: [
+    JugadorComponent,
+    MatButtonToggle,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatPaginator,
+    MatSelect,
+    NgIf,
+    ReactiveFormsModule,
+    NgForOf,
+    FormatoNumeroPipe,
+    DatePipe
+  ],
   templateUrl: './mercado-use.component.html',
   styleUrl: './mercado-use.component.css'
 })
-export class MercadoUseComponent {
+export class MercadoUseComponent implements OnInit {
+  jugadoresEnVenta: any[] = [];
+  searchControl = new FormControl('');
+  filterForm: FormGroup;
+  totalJugadores: number = 0;
+  pageSize: number = 12;
+  pageIndex: number = 0;
+
+  constructor(private mercadoService: MercadoService) {
+    this.filterForm = new FormGroup({
+      equipo: new FormControl(''),
+      rareza: new FormControl(''),
+      posicion: new FormControl(''),
+      valor_min: new FormControl(''),
+      valor_max: new FormControl(''),
+      ordering: new FormControl('')
+    });
+  }
+
+  ngOnInit(): void {
+    this.getMercadoUserList();
+
+    this.searchControl.valueChanges.subscribe(value => {
+      this.pageIndex = 0;
+      this.getMercadoUserList(value ?? undefined, this.filterForm.value);
+    });
+
+    this.filterForm.valueChanges.subscribe(value => {
+      this.pageIndex = 0;
+      this.getMercadoUserList(this.searchControl.value ?? undefined, value);
+    });
+  }
+
+  getMercadoUserList(search?: string, filters?: any): void {
+    this.mercadoService.getMercadoUsuario(search, filters, this.pageIndex + 1, this.pageSize).subscribe(response => {
+      this.jugadoresEnVenta = response.results;
+      this.totalJugadores = response.count;
+    });
+  }
+
+  limpiarFiltros(): void {
+    this.filterForm.reset();
+    this.searchControl.reset();
+    this.pageIndex = 0;
+    this.getMercadoUserList();
+
+  }
+
+  handlePageEvent(event: PageEvent): void {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.getMercadoUserList(this.searchControl.value ?? undefined, this.filterForm.value);
+  }
 
 }
