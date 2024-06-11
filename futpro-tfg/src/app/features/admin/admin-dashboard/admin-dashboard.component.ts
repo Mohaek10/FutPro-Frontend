e1ada3137f0c0d36fbb93fc723f6f752d865c0ab
Mohaek10 <elkasmimoha@gmail.com>
2024-06-11 08:43:47 +0200

    Footer y header mi equipo

--- futpro-tfg/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
+++ futpro-tfg/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {MatButton} from "@angular/material/button";
+import {FooterComponent} from "../../../layout/footer/footer.component";
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
   imports: [
-    MatButton
+    MatButton,
+    FooterComponent
   ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
