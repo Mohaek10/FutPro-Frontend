0dd24a70eafa11e57b4b860c16a1524f6d634b3a
Mohaek10 <elkasmimoha@gmail.com>
2024-06-11 22:47:59 +0200

    Registro hecho

--- futpro-tfg/src/app/features/auth/register/register.component.ts
+++ futpro-tfg/src/app/features/auth/register/register.component.ts
@@ -1,45 +1,99 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../../core/services/auth.service";
-import {Router} from "@angular/router";
-import {FormsModule} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule, ValidationErrors,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    RouterLink,
+    MatFormField,
+    MatInput,
+    MatCheckbox,
+    MatError,
+    MatButton,
+    MatLabel,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  username: string = '';
-  email: string = '';
-  first_name: string = '';
-  last_name: string = '';
-  phone_number: string = '';
-  password: string = '';
-  password2: string = '';
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3), NoWhitespaceValidator()]],
+      email: ['', [Validators.required, Validators.email]],
+      first_name: ['', [Validators.required, Validators.minLength(3), NoWhitespaceValidator()]],
+      last_name: ['', [Validators.required, Validators.minLength(3), NoWhitespaceValidator()]],
+      phone_number: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/.*\d.*/)]], // Añadido el validador de patrón
+      password2: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue]
+    }, {validators: this.validacionConfirm});
   }
 
-  onSubmit(): void {
-    const user = {
-      username: this.username,
-      email: this.email,
-      first_name: this.first_name,
-      last_name: this.last_name,
-      phone_number: this.phone_number,
-      password: this.password,
-      password2: this.password2
-    };
+  validacionConfirm(form: FormGroup) {
+    return form.get('password')?.value === form.get('password2')?.value
+      ? null : {'mismatch': true};
+  }
 
-    this.authService.register(user).subscribe({
-      next: () => this.router.navigate(['/']),
-      error: err => console.error('Error en el registro', err)
-    });
+  register(): void {
+    if (this.registerForm.valid) {
+      const user = this.registerForm.value;
+      console.log(user);
+      this.authService.register(user).subscribe({
+        next: () => {
+          this.snackBar.open('Registro exitoso', 'Cerrar', {duration: 3000});
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          const errorMessages = this.parseErrorMessages(err);
+          this.snackBar.open(errorMessages, 'Cerrar', {duration: 5000});
+        }
+      });
+    }
+  }
+
+  private parseErrorMessages(err: any): string {
+    if (err.error) {
+      if (typeof err.error === 'string') {
+        return err.error;
+      } else if (typeof err.error === 'object') {
+        return Object.values(err.error).map((error: any) => error.join(' ')).join(' ');
+      }
+    }
+    return 'Error al crear el jugador. Verifica los datos.';
   }
 }
 
-
+export function NoWhitespaceValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : {'whitespace': true};
+  };
+}
