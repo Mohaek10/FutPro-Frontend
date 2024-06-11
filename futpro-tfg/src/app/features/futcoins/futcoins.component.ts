926460ee1f4259292f91e1bac7f8903c66e8b52a
Mohaek10 <elkasmimoha@gmail.com>
2024-06-11 18:23:17 +0200

    Futcoins

---
+++ futpro-tfg/src/app/features/futcoins/futcoins.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-futcoins',
+  standalone: true,
+  imports: [],
+  templateUrl: './futcoins.component.html',
+  styleUrl: './futcoins.component.css'
+})
+export class FutcoinsComponent {
+
+}
